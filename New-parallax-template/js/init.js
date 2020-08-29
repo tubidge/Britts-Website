6c6696411f712bca31e9b0ad281a300cc1ae973c
tubidge <tubidge@gmail.com>
2020-08-29 15:11:47 -0600

    updating images

--- New-parallax-template/js/init.js
+++ New-parallax-template/js/init.js
@@ -3,7 +3,9 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    // $('.scrollspy').scrollSpy();
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 0
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
